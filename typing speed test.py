40411d3a46a71e684c0e62dbe01475754ca07885
Divyansh Singh <61814592+Divyanshpythonist@users.noreply.github.com>
2020-03-28 01:25:21 +0530

    Update typing speed test.py

--- typing speed test.py
+++ typing speed test.py
@@ -1,6 +1,6 @@
 import random
 import time
-import timeit
+#import timeit
 print("TYPING SPEED TEST")
 k=0
 random_sentences=['The fish listened intently to what the frogs had to say.','It was the best sandcastle he had ever seen.','Wisdom is easily acquired when hiding under the bed with a saucepan on your head.',"The elephant didn't want to talk about the person in the room.",'She traveled because it cost the same as therapy and was a lot more enjoyable.','When transplanting seedlings, candied teapots will make the task easier.',"Now I need to ponder my existence and ask myself if I'm truly real",'Never underestimate the willingness of the greedy to throw you under the bus.','There were white out conditions in the town; subsequently, the roads were impassable.','The Japanese yen for commerce is still well-known.']
